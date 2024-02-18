876e4c3ffbd66622adabac7540b68cb769971d46
Louie Cervantes <128763658+louiecervantes@users.noreply.github.com>
2024-02-18 09:59:15 +0800

    save

--- streamlit_app.py
+++ streamlit_app.py
@@ -73,11 +73,10 @@ def app():
         ax.set_title('Plot of Sex Distribution')
 
         # Add the counts to the bars
-        for i, p in enumerate(ax.patches):
-            if i > 0:  # Skip the first bar
-                ax.annotate(f'{p.get_width():,.0f}',  # Format count as integer
-                            (p.get_x() + p.get_width() / 2., p.get_y() + p.get_height() / 2.),
-                            ha='center', va='center', fontsize=10, fontweight='bold')
+        for p in enumerate(ax.patches):
+            ax.annotate(f'{p.get_width():,.0f}',  # Format count as integer
+                        (p.get_x() + p.get_width() / 2., p.get_y() + p.get_height() / 2.),
+                        ha='center', va='center', fontsize=10, fontweight='bold')
 
         # Display the plot using Streamlit
         st.pyplot(fig)
